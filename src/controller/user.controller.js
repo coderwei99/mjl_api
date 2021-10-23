1048dae4ccf8c248316db999cba9385fa75184c8
杜伟 <duwei@qq.com>
2021-10-24 01:35:41 +0800

    导入koa-body，解析网络请求携带的参数

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,6 +1,10 @@
+const { createUser } = require("../server/user.server");
 class UserController {
   async register(ctx, next) {
-    ctx.body = "hello uses 用户已注册";
+    const { user_name, password } = ctx.request.body;
+    const res = await createUser(user_name, password);
+    console.log(res);
+    ctx.body = res;
   }
   async login(ctx, next) {
     ctx.body = "hello uses 用户已登录";
---
+++ src/server/user.server.js
@@ -0,0 +1,7 @@
+class UserServer {
+  async createUser(user_name, password) {
+    return "写入数据库成功";
+  }
+}
+
+module.exports = new UserServer();
