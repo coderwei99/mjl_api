d37fb79b3a54555e52bda6773953d37efe52fb3c
杜伟 <duwei@qq.com>
2021-10-24 05:06:17 +0800

    用户注册后台验证合法性

--- src/server/user.server.js
+++ src/server/user.server.js
@@ -1,10 +1,25 @@
 const User = require("../model/user_model");
 
 class UserServer {
+  // 创建一条用户数据
   async createUser(user_name, password) {
-    // return "写入数据库成功";
     const res = await User.create({ user_name, password });
     return res.dataValues;
+  }
+
+  // 查找用户
+  async getUserInfo({ id, user_name, password, is_anmin }) {
+    const whereOpt = {};
+    id && Object.assign(whereOpt, { id });
+    user_name && Object.assign(whereOpt, { user_name });
+    password && Object.assign(whereOpt, { password });
+    is_anmin && Object.assign(whereOpt, { is_anmin });
+    const res = User.findOne({
+      attributes: ["id", "password", "is_admin", "user_name"],
+      where: whereOpt,
+    });
+
+    return res ? res.dataValues : null;
   }
 }
 
