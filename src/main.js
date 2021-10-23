82f492a9daf2daac6cc9a389437f627296fe00ad
杜伟 <duwei@qq.com>
2021-10-24 00:32:49 +0800

    项目的基本优化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 const Koa = require("koa");
 
+const { APP_POST } = require("./config/config.default");
+
 const app = new Koa();
 
 app.use((cts, next) => {
-  cts.body = "hello api";
+  cts.body = "hello api11asdasdsa";
 });
-app.listen(3000, () => {
-  console.log("server is running on http://localhost:3000");
+
+app.listen(APP_POST, () => {
+  console.log(`server is running on http://localhost:${APP_POST}`);
 });
