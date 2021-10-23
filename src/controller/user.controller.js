0e2e7ee92f187765e883d532bd610a571168f51f
杜伟 <duwei@qq.com>
2021-10-24 04:38:41 +0800

    注册用户成功返回

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,7 +4,14 @@ class UserController {
     const { user_name, password } = ctx.request.body;
     const res = await createUser(user_name, password);
     console.log(res);
-    ctx.body = res;
+    ctx.body = {
+      code: 0,
+      message: "用户注册成功",
+      result: {
+        id: res.id,
+        user_name: res.user_name,
+      },
+    };
   }
   async login(ctx, next) {
     ctx.body = "hello uses 用户已登录";
--- src/server/user.server.js
+++ src/server/user.server.js
@@ -1,6 +1,10 @@
+const User = require("../model/user_model");
+
 class UserServer {
   async createUser(user_name, password) {
-    return "写入数据库成功";
+    // return "写入数据库成功";
+    const res = await User.create({ user_name, password });
+    return res.dataValues;
   }
 }
 
