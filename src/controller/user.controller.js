95b3af1e9d2387d824f370794fab2c032d80ebbb
杜伟 <duwei@qq.com>
2021-10-24 19:09:55 +0800

    用户登录颁发token

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,9 @@
-const { createUser } = require("../server/user.server");
+const jwt = require("jsonwebtoken");
+
+const { createUser, getUserInfo } = require("../server/user.server");
 const { userRegisterError } = require("../consitant/error.type");
+
+const { JWT_SECRET } = require("../config/config.default");
 class UserController {
   async register(ctx, next) {
     const { user_name, password } = ctx.request.body;
@@ -20,7 +24,20 @@ class UserController {
   }
   async login(ctx, next) {
     const { user_name, password } = ctx.request.body;
-    ctx.body = `hello  ${user_name}`;
+    // ctx.body = `hello  ${user_name}`;
+    // 获取用户信息
+    try {
+      const { password, ...res } = await getUserInfo({ user_name });
+      ctx.body = {
+        code: 0,
+        message: "用户登录成功",
+        result: {
+          token: jwt.sign(res, JWT_SECRET, { expiresIn: "1d" }),
+        },
+      };
+    } catch (err) {
+      console.error("用户登录失败", err);
+    }
   }
 }
 
