c4c649d89e3c28cebf64ff89c64e575d6713a638
coderwei <916606569@qq.com>
2022-10-22 03:21:55 +0800

    fix: 修改发送模板消息请求成功返回unfettered

--- src/request/module/template.js
+++ src/request/module/template.js
@@ -17,42 +17,26 @@ function getAccessToken() {
 
 // 进行消息推送
 
-function postSendTemplate(access_token, data) {
+async function postSendTemplate(access_token, data) {
   // 1.
-  axios
-    .post(
-      `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${access_token}`,
-      {
-        touser,
-        template_id,
-        url,
-        data,
-        // "miniprogram": {
-        //   "appid": "wx65550dc45bb78a1f",
-        //   "pagepath": "index"
-        // },
-      }
-    )
-    .then(res => {
-      console.log("res", res);
-      // TODO 判断res的code  发送失败也要返回false
-      return true;
-    })
-    .catch(function (error) {
-      console.log("error===============", error);
-      return false;
-    });
-  // 2.
-  // return TripRequest.post({
-  //   // url: `cgi-bin/message/template/send?access_token=61_vxw5THRM6yR_xc-z3SZs64RyZ41FkdZP_Ncha351bCBDoY5GirBZrueGrFXp3Y8h8W0rIznnKfEXswlna5B9nf-1XqYaNq-g9bdblbsMkjtyUBXVqOE-NbWXgRjzD8lKiSzOe7u0MMNblgmrPNBbACAJHV`,
-  //   url: `cgi-bin/message/template/send?access_token=${access_token}`,
-  //   data: {
-  //     touser,
-  //     template_id,
-  //     url,
-  //     data: datas
-  //   },
-  // })
+  const res = await axios.post(
+    `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${access_token}`,
+    {
+      touser,
+      template_id,
+      url,
+      data,
+      // "miniprogram": {
+      //   "appid": "wx65550dc45bb78a1f",
+      //   "pagepath": "index"
+      // },
+    }
+  );
+  if (res.data.errcode == 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
